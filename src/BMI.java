355473a9cc7daac701fae4932d45d0dccc5e1fc0
yousra bibi <ybibi@ggc.edu>
2023-02-09 22:56:03 -0500

    added comment line.

--- src/BMI.java
+++ src/BMI.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class BMI {
+/**
+ * @class: BMI
+ * @author: Yousra bibi
+ * @course: ITEC 2140 -05, 2023
+ * @version: 1.0
+ * @date: 02/09/2023
+ * @description: this program will calculate the body mass index based on the height and weight of the person.
+ */
+
+import java.util.Scanner;
+
+public class BMI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your weight in pounds: ");
+        double weight = 0.45359237 * sc.nextDouble();
+        System.out.println("Enter your height in inches: ");
+        double height = 0.0254 * sc.nextDouble();
+        double BMI = weight / (height * height);
+        System.out.println("your BMI is: " + BMI);
+        if (BMI < 18.5)
+        {
+            System.out.println("You are underweight");
+        }
+        else if (BMI > 18.5 && BMI < 25.0)
+        {
+            System.out.println("You are Normal");
+        }
+        else if (BMI > 25.0 && BMI < 30.0)
+        {
+            System.out.println("You are Overweight");
+        }
+        else
+        {
+            System.out.println("You are Obese");
+        }
+
+    }
+
+
 }
+
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Triangle {
-}
+/**
+ * @class: Triangle
+ * @author: Yousra bibi
+ * @course: ITEC 2140 -05, 2023
+ * @version: 1.0
+ * @date: 02/09/2023
+ * @description: this program will calculate the perimeter of a triangle.
+ */
+import java.util.Scanner;
+
+    public class Triangle {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the length of edges: ");
+            double side1 = sc.nextDouble();
+            double side2 = sc.nextDouble();
+            double side3 = sc.nextDouble();
+            if(side1 < side2 + side3 && side2 < side1 + side3 && side3 < side1 + side2 )
+            {
+                double perimeter = side1 + side2 + side3;
+                System.out.println("Perimeter: " + perimeter);
+            }
+            else {
+                System.out.println("Invalid inputs");
+            }
+        }
+    }
+
